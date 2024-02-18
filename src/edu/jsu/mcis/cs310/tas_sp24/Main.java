3726265acbf14928dd241e6848caebdbf4963631
Jay Snellen, III <jsnellen@jsu.edu>
2024-02-18 17:46:43 -0600

    Initial Commit

---
+++ src/edu/jsu/mcis/cs310/tas_sp24/Main.java
@@ -0,0 +1,28 @@
+package edu.jsu.mcis.cs310.tas_sp24;
+
+import edu.jsu.mcis.cs310.tas_sp24.dao.*;
+import java.time.*;
+import java.util.*;
+import java.time.temporal.ChronoUnit;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        // test database connectivity; get DAOs
+
+        DAOFactory daoFactory = new DAOFactory("tas.jdbc");
+        BadgeDAO badgeDAO = daoFactory.getBadgeDAO();
+        
+        // find badge
+
+        Badge b = badgeDAO.find("C4F37EFF");
+        
+        // output should be "Test Badge: #C4F37EFF (Welch, Travis C)"
+        
+        System.err.println("Test Badge: " + b.toString());
+
+    }
+
+}
