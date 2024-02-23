7c64b2bfff59b784617c7b4f090ac3ba5c5c6975
RyanCan'tCode <meubanks2@stu.jsu.edu>
2024-02-23 12:40:04 -0600

    Employee model class and EmployeeDAO created Shift model class an ShiftDAO created

---
+++ src/edu/jsu/mcis/cs310/tas_sp24/Employee.java
@@ -0,0 +1,24 @@
+package edu.jsu.mcis.cs310.tas_sp24;
+
+import java.time.LocalDateTime;
+
+public class Employee {
+
+    private String id;
+    private String firstName, middleName, lastName;
+
+    // start date
+    LocalDateTime active;
+    Badge employeeBadge;
+
+    //Department employeeDepartment;  not implemented yet
+
+    // representing the shift rule set for the employee's assigned work shift
+    //Shift rules; not implemented
+
+    public Employee(String firstName, String middleName, String lastName, String id) {
+
+    }
+
+
+}
