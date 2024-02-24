3945267cb193df5c0571d482b7223a2add8511fd
Kristopher McAnally <kristopher.mcanally@gmail.com>
2024-02-23 21:15:02 -0600

    Added Shift Constructor and Getter Methods Created General DAO structure for Shift

--- src/edu/jsu/mcis/cs310/tas_sp24/Shift.java
+++ src/edu/jsu/mcis/cs310/tas_sp24/Shift.java
@@ -1,4 +1,76 @@
 package edu.jsu.mcis.cs310.tas_sp24;
 
+import java.time.LocalTime;
+import java.util.HashMap;
+
 public class Shift {
+    
+    private final String description;
+    private final int id, roundInterval, gracePeriod, dockPenalty, lunchThreshold, shiftDuration, lunchDuration;
+    private final LocalTime shiftStart, shiftStop, lunchStart, lunchStop;
+    
+    public Shift(HashMap shift) {
+        this.id = 0;
+        this.description = "";
+        this.shiftStart = null;
+        this.shiftStop = null;
+        this.roundInterval = 0;
+        this.gracePeriod = 0;
+        this.dockPenalty = 0;
+        this.lunchStart = null;
+        this.lunchStop = null;
+        this.lunchThreshold = 0;
+        this.shiftDuration = 0;
+        this.lunchDuration = 0;
+    }
+    
+    public String getDescription() {
+        return this.description;
+    }
+    
+    public int getId() {
+        return this.id;
+    }
+    
+    public int getRoundInterval() {
+        return this.roundInterval;
+    }
+    
+    public int getGracePeriod() {
+        return this.gracePeriod;
+    }
+    
+    public int getDockPenalty() {
+        return this.dockPenalty;
+    }
+    
+    public int getLunchThreshold() {
+        return this.lunchThreshold;
+    }
+    
+    public int getShiftDuration() {
+        return this.shiftDuration;
+    }
+    
+    public int getLunchDuration() {
+        return this.lunchDuration;
+    }
+    
+    public LocalTime getShiftStart() {
+        return this.shiftStart;
+    }
+    
+    public LocalTime getShiftStop() {
+        return this.shiftStop;
+    }
+    
+    public LocalTime getLunchStart() {
+        return this.lunchStart;
+    }
+    
+    public LocalTime getLunchStop() {
+        return this.lunchStop;
+    }
+    
+    //toString override incoming
 }
--- src/edu/jsu/mcis/cs310/tas_sp24/dao/ShiftDAO.java
+++ src/edu/jsu/mcis/cs310/tas_sp24/dao/ShiftDAO.java
@@ -1,4 +1,134 @@
 package edu.jsu.mcis.cs310.tas_sp24.dao;
 
+import edu.jsu.mcis.cs310.tas_sp24.Badge;
+import edu.jsu.mcis.cs310.tas_sp24.Shift;
+import java.sql.*;
+
 public class ShiftDAO {
+
+    private static final String QUERY_FIND = "SELECT * FROM badge WHERE id = ?";
+
+    private final DAOFactory daoFactory;
+
+    ShiftDAO(DAOFactory daoFactory) {
+
+        this.daoFactory = daoFactory;
+
+    }
+
+    public Shift find(int id) {
+
+        Shift shift = null;
+
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+
+            Connection conn = daoFactory.getConnection();
+
+            if (conn.isValid(0)) {
+
+                ps = conn.prepareStatement(QUERY_FIND);
+                ps.setInt(1, id);
+
+                boolean hasresults = ps.execute();
+
+                if (hasresults) {
+
+                    rs = ps.getResultSet();
+
+                    while (rs.next()) {
+
+                        // stuff
+                    }
+
+                }
+
+            }
+
+        } catch (SQLException e) {
+
+            throw new DAOException(e.getMessage());
+
+        } finally {
+
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+
+        }
+
+        return shift;
+
+    }
+    
+    public Shift find(Badge badge) {
+
+        Shift shift = null;
+
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+
+            Connection conn = daoFactory.getConnection();
+
+            if (conn.isValid(0)) {
+
+                ps = conn.prepareStatement(QUERY_FIND);
+                ps.setString(1, badge.getId());
+
+                boolean hasresults = ps.execute();
+
+                if (hasresults) {
+
+                    rs = ps.getResultSet();
+
+                    while (rs.next()) {
+
+                        // stuff
+                    }
+
+                }
+
+            }
+
+        } catch (SQLException e) {
+
+            throw new DAOException(e.getMessage());
+
+        } finally {
+
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+
+        }
+
+        return shift;
+
+    }
 }
