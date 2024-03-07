8f1d21eace35403bdc8c46d9664d036def1fffd6
sboswell5 <shelbyjboswell@gmail.com>
2024-03-07 17:20:26 -0600

    worked on the adjust method, have a question :)

--- src/edu/jsu/mcis/cs310/tas_sp24/Punch.java
+++ src/edu/jsu/mcis/cs310/tas_sp24/Punch.java
@@ -12,8 +12,8 @@ public class Punch {
     private int id;
     private final Badge badge;
     private final EventType punchType;
-    private LocalDateTime originalTimestamp, adjustedTimestamp;
-    private PunchAdjustmentType adjustmentType;
+    private LocalDateTime originalTimestamp, adjustedtimestamp;
+    private PunchAdjustmentType adjustmenttype;
 
     // Minimal constructor for Punch objects
     // Added originalTimestamp
@@ -34,6 +34,93 @@ public class Punch {
         this.originalTimestamp = originalTimestamp;
         this.punchType = punchType; 
     }
+    
+    public void adjust(Shift s){
+                       
+        //defining our variables: 
+        
+       LocalTime shiftstart = s.getShiftStart();
+       
+       LocalTime shiftstop = s.getShiftStop();
+       
+       LocalTime lunchstart = s.getLunchStart();
+       
+       LocalTime lunchstop = s.getLunchStop();
+       
+       int roundInterval = s.getRoundInterval();
+       
+       int gracePeriod = s.getGracePeriod();
+       
+       int dockPenalty = s.getDockPenalty();
+       //if it is a weekday
+       if(isWeekend(originalTimestamp) == false){
+           
+           LocalDate placeholder = LocalDate.from(originalTimestamp);
+           //if we are clocking in for a start of shift
+           if(punchType == punchType.CLOCK_IN){
+               
+               if(originalTimestamp.toLocalTime().isBefore(shiftstart)){
+                   
+                   adjustedtimestamp = LocalDateTime.of(placeholder, shiftstart);
+                   
+               }
+               else if(originalTimestamp.toLocalTime().isAfter(shiftstart.plusMinutes(gracePeriod))){
+               
+                    adjustedtimestamp = LocalDateTime.of(placeholder, originalTimestamp.toLocalTime().plusMinutes(dockPenalty));
+           }
+          }
+          //if we are clocking out at the end of shift 
+          if(punchType == punchType.CLOCK_OUT){
+              if(originalTimestamp.toLocalTime().isBefore(shiftstop.minusMinutes(gracePeriod))){
+                  adjustedtimestamp = LocalDateTime.of(placeholder, originalTimestamp.toLocalTime().minusMinutes(dockPenalty));
+              }
+              else if(originalTimestamp.toLocalTime().isAfter(shiftstop)){
+                  adjustedtimestamp = LocalDateTime.of(placeholder, shiftstop);
+              }
+            //not sure if this is right, how do we handle lunches??
+          }
+          //if we are clocking out for lunch? what does time out mean :)
+          if(punchType == punchType.TIME_OUT){
+              if(originalTimestamp.toLocalTime().isBefore(lunchstart) || originalTimestamp.toLocalTime().isAfter(lunchstart)){
+                  adjustedtimestamp = LocalDateTime.of(placeholder, lunchstart);
+              }
+           //same logic for clocking back in after lunch: what is the unique identifier for this? 
+          }
+       }
+       
+       
+       
+       
+        
+        //get badge
+        
+        //determine if they are on break or actually clocking out for the day 
+        
+        //get localDateTime of the shift they're on, figure out how to determine if they need to be penalized or are in the grace period
+        
+        //result is saved as adjustedTimeStamp of localDateTime type
+ 
+        
+        
+        
+    }
+    
+    //creating a function to determine if it's the weekend or not: 
+    
+    public boolean isWeekend(LocalDateTime day){
+        
+        boolean weekend = false;
+        
+        if(day.getDayOfWeek().toString().equals("SATURDAY") || day.getDayOfWeek().toString().equals("SUNDAY")){
+            
+            weekend = true;
+            
+        }
+        
+       return weekend; 
+        
+    }
+    
     
     public int getTerminalid() {
         
