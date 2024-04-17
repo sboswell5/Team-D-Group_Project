0bf270d77b0c83123e2dd9dfc4401c274e698e10
RyanCan'tCode <meubanks2@stu.jsu.edu>
2024-04-17 13:32:05 -0500

    shift fixed
    all v1 tests pass

--- src/edu/jsu/mcis/cs310/tas_sp24/Shift.java
+++ src/edu/jsu/mcis/cs310/tas_sp24/Shift.java
@@ -5,35 +5,14 @@ import java.util.HashMap;
 
 public class Shift {
     
-    //private final String description;
-    //private final Integer id, roundInterval, gracePeriod, dockPenalty, lunchThreshold;
-    //private final Duration shiftDuration, lunchDuration;
-    //private final LocalTime shiftStart, shiftStop, lunchStart, lunchStop;
-    //private final DailySchedule defaultschedule;
-    
     private final String description;
     private final Integer id;
     private final DailySchedule defaultschedule;
     
-    /*public Shift(HashMap<String, String> shift) {
-        this.id = Integer.valueOf(shift.get("id"));
-        this.description = shift.get("description");
-        this.shiftStart = LocalTime.parse(shift.get("shiftStart"));
-        this.shiftStop = LocalTime.parse(shift.get("shiftStop"));
-        this.roundInterval = Integer.valueOf(shift.get("roundInterval"));
-        this.gracePeriod = Integer.valueOf(shift.get("gracePeriod"));
-        this.dockPenalty = Integer.valueOf(shift.get("dockPenalty"));
-        this.lunchStart = LocalTime.parse(shift.get("lunchStart"));
-        this.lunchStop = LocalTime.parse(shift.get("lunchStop"));
-        this.lunchThreshold = Integer.valueOf(shift.get("lunchThreshold"));
-        this.shiftDuration = Duration.parse(shift.get("shiftDuration"));
-        this.lunchDuration = Duration.parse(shift.get("lunchDuration"));
-    }*/
-    
     public Shift(HashMap<String, String> shift, DailySchedule dailySchedule) {
         this.id = Integer.valueOf(shift.get("id"));
         this.description = shift.get("description");
-        this.defaultschedule = new DailySchedule(shift);
+        this.defaultschedule = dailySchedule;
     }
     
     public String getDescription() {
