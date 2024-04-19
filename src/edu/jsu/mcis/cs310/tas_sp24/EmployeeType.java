05410ca2c4138fb8ff0b718388acb2876a97c3f2
Kristopher McAnally <kristopher.mcanally@gmail.com>
2024-04-19 09:49:12 -0500

    Updated EmployeeType javadoc

--- src/edu/jsu/mcis/cs310/tas_sp24/EmployeeType.java
+++ src/edu/jsu/mcis/cs310/tas_sp24/EmployeeType.java
@@ -7,14 +7,26 @@ package edu.jsu.mcis.cs310.tas_sp24;
 
 public enum EmployeeType {
 
+    /**
+     * Two employee types, full or part time
+     * description - employeeType
+     */
     PART_TIME("Temporary / Part-Time"),
     FULL_TIME("Full-Time");
     private final String description;
 
+    /**
+     * Establishes the EmployeeType enum
+     * @param d passes the employeeType
+     */
     private EmployeeType(String d) {
         description = d;
     }
 
+    /**
+     * Ovverrides the toString method
+     * @return employeeType
+     */
     @Override
     public String toString() {
         return description;
