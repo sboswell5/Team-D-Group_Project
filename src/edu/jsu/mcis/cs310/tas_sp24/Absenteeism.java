8562126fdbaaf7c3779753264a445e1a6e8c16bd
RyanCan'tCode <meubanks2@stu.jsu.edu>
2024-04-19 12:07:59 -0500

    basic javadocs
    not too much info in the absenteeism class

--- src/edu/jsu/mcis/cs310/tas_sp24/Absenteeism.java
+++ src/edu/jsu/mcis/cs310/tas_sp24/Absenteeism.java
@@ -3,26 +3,49 @@ package edu.jsu.mcis.cs310.tas_sp24;
 import java.time.LocalDate;
 import java.math.BigDecimal;
 
+/**
+ *
+ * @author Madelyn
+ */
+
 public class Absenteeism {
 
     private final Employee employee;
     private final LocalDate localDate;
     private final BigDecimal bigDecimal;
 
+    /**
+     *
+     * @param employee
+     * @param localDate
+     * @param bigDecimal
+     */
     public Absenteeism(Employee employee, LocalDate localDate, BigDecimal bigDecimal) {
         this.employee = employee;
         this.localDate = localDate;
         this.bigDecimal = bigDecimal;
     }
 
+    /**
+     *
+     * @return employee
+     */
     public Employee getEmployee() {
         return employee;
     }
 
+    /**
+     *
+     * @return localDate
+     */
     public LocalDate getLocalDate() {
         return localDate;
     }
-    
+
+    /**
+     *
+     * @return bigDecimal
+     */
     public BigDecimal getBigDecimal() {
         return bigDecimal;
     }
