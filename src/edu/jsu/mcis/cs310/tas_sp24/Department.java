a327c02a2e74233550218a4fc80ab8243e431433
Arthur Duvall <artieduvall@yahoo.com>
2024-04-19 16:40:24 -0500

    Added javadoc for department and departmentdao

--- src/edu/jsu/mcis/cs310/tas_sp24/Department.java
+++ src/edu/jsu/mcis/cs310/tas_sp24/Department.java
@@ -1,39 +1,63 @@
 package edu.jsu.mcis.cs310.tas_sp24;
 
+/**
+ * The <code>Department</code> class represents a department in the TAS system.
+ * It encapsulates information about the department, including its ID, description, and terminal ID.
+ */
 public class Department {
 
     private final int id, terminalid;
     private final String description;
     
+    /**
+     * Constructs a new <code>Department</code> instance with the specified ID, description, and terminal ID.
+     * @param id the ID of the department
+     * @param description the description of the department
+     * @param terminalid the terminal ID of the department
+     */
     public Department(int id, String description, int terminalid) {
         this.id = id;
         this.description = description;
         this.terminalid = terminalid;
     }
     
+    /**
+     * Returns the ID of the department.
+     * @return the ID of the department
+     */
     public int getId() {
         return id;
     }
     
+    /**
+     * Returns the description of the department.
+     * @return the description of the department
+     */
     public String getDescription() {
         return description;
     }
     
+    /**
+     * Returns the terminal ID of the department.
+     * @return the terminal ID of the department
+     */
     public int getTerminalid() {
         return terminalid;
     }
     
+    /**
+     * Returns a string representation of the department.
+     * The string representation includes the department ID, description, and terminal ID.
+     * @return a string representation of the department
+     */
     @Override
     public String toString() {
-
         StringBuilder s = new StringBuilder();
-
         s.append('#').append(id).append(' ');
         s.append('(').append(description).append(')');
         s.append(", Terminal ID: ").append(terminalid);
-        
         return s.toString();
-    
     }
     
 }
+
--- src/edu/jsu/mcis/cs310/tas_sp24/dao/DepartmentDAO.java
+++ src/edu/jsu/mcis/cs310/tas_sp24/dao/DepartmentDAO.java
@@ -3,59 +3,54 @@ package edu.jsu.mcis.cs310.tas_sp24.dao;
 import edu.jsu.mcis.cs310.tas_sp24.Department;
 import java.sql.*;
 
+/**
+ * The <code>DepartmentDAO</code> class provides methods to interact with the department database table.
+ * It allows for the retrieval of department information based on department ID.
+ */
 public class DepartmentDAO {
     
+    // Query to find department by ID
     private static final String QUERY_FIND = "SELECT * FROM department WHERE id = ?";   
     
     private final DAOFactory daoFactory;
 
-    
+    /**
+     * Constructs a new <code>DepartmentDAO</code> instance with the specified DAOFactory.
+     * @param daoFactory the DAOFactory to be used by this DepartmentDAO
+     */
     DepartmentDAO(DAOFactory daoFactory) {
-        
         this.daoFactory = daoFactory;
-        
     }
     
+    /**
+     * Finds and retrieves a department from the database based on the provided department ID.
+     * @param id the ID of the department to be retrieved
+     * @return the Department object retrieved from the database, or null if no department was found
+     * @throws DAOException if there is an error accessing the database
+     */
     public Department find(int id) {
-        
         Department department = null;
-        
         PreparedStatement ps = null;
         ResultSet rs = null;
-        
         try {
-
             Connection conn = daoFactory.getConnection();
-
             if (conn.isValid(0)) {
-
                 ps = conn.prepareStatement(QUERY_FIND);
                 ps.setInt(1, id);
-
-                boolean hasresults = ps.execute();
-
-                if (hasresults) {
-
+                boolean hasResults = ps.execute();
+                if (hasResults) {
                     rs = ps.getResultSet();
-
                     while (rs.next()) {
-
                         String description = rs.getString("description");
                         int terminalid = rs.getInt("terminalid");
                         department = new Department(id, description, terminalid);
-
                     }
-
                 }
-
             }
-
         } catch (SQLException e) {
-
             throw new DAOException(e.getMessage());
-
         } finally {
-
+            // Close resources
             if (rs != null) {
                 try {
                     rs.close();
